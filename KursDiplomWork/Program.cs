835a81cac1ef052f511d750397bbd177ae005756
Skiba.Evgeniy <Skiba.Evgeniy@hotmail.com>
2016-04-25 15:43:13 +0300

    создал проект

---
+++ KursDiplomWork/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursDiplomWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
