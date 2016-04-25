7e0b369363162ff3ee2387521426f4f242db5e13
Skiba.Evgeniy <Skiba.Evgeniy@hotmail.com>
2016-04-25 15:48:20 +0300

    создал базовый класс работа

---
+++ KursDiplomWork/Work.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KursDiplomWork
+{
+    public abstract  class Work
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string StudentName { get; set; }
+        public string GroupNumber { get; set; }
+    }
+}
